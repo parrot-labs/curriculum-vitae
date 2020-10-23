05463f60737a9513b7145c97636c38afa447af6f
parrot-labs <parrot-labs@github.com>
2020-10-23 09:10:25 +0700

    second commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import {Link} from 'react-router-dom'
+import './Sticky.scss';
 
 const Navbar = () => {
     const listMenu = [ 'Home', 'About', 'Education', 'Skills', 'Contact'];
+    const [isSticky, setSticky] = useState(false);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', () => handleScroll);
+    };
+
+  }, []);
+
+  const handleScroll = () => {
+    if (ref && ref.current && ref.current.getBoundingClientRect()) {
+        setSticky(ref.current.getBoundingClientRect().top);
+    }
+  };
+
     return (
+      <div className={`sticky__wrapper ${isSticky && 'sticky'}`} ref={ref}>
+      <div className="sticky--inner"></div>
       <div className="header">
         {listMenu.map((name) => {
           return (
@@ -12,6 +32,7 @@ const Navbar = () => {
             </Link>
           );
         })}
+      </div>
       </div>
     );
   };
